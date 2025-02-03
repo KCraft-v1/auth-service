8b87ce51dee6b9af90a2861d346a66b94e17f7f3
exception-raised <dimmelman@riseup.net>
2025-02-03 19:56:22 +0200

    Rust cleanup

--- src/handlers/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod register;
-
-pub use register::handle_register_user;
--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use auth_service::server;
-
-#[tokio::main]
-async fn main() {
-    println!("Server started at: http://localhost:8080");
-    warp::serve(server::register_routes())
-        .run(([127, 0, 0, 1], 8080))
-        .await;
-}
